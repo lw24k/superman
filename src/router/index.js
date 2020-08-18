41622d35be741e8d8443bad72b5c2f835a6a51ad
lw24k <lw24k@126.com>
2020-08-18 23:15:22 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,28 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const home = () => import('views/home')
+const cart = () => import('views/cart')
+const profile = () => import('views/profile')
+const category = () => import('views/category')
 
+const routes = [
+  {
+    path: '/home',
+    component: home
+  },
+  {
+    path: '/category',
+    component: category
+  },
+  {
+    path: '/cart',
+    component: cart
+  },
+  {
+    path: '/profile',
+    component: profile
+  }
 ]
 
 const router = new VueRouter({
